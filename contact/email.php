692fd6698683dec0a9fe2ea506c95462ecbb6408
Rodrigo Botelho <rodrigo.botelho@innersite.com.br>
2017-03-22 16:08:26 -0300

    envio de sms adicionado api direct call

--- contact/email.php
+++ contact/email.php
@@ -1,5 +1,22 @@
 <?php
 
+// ENVIO DE SMS
+	function requisicaoApi($params, $endpoint){
+		$url = "http://api.directcallsoft.com/{$endpoint}";
+		$data = http_build_query($params);
+		$ch =   curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		$return = curl_exec($ch);
+		curl_close($ch);
+		// Converte os dados de JSON para ARRAY<
+		$dados = json_decode($return, true);
+		return $dados;
+    }
+
 
 header('Content-type: application/json');
  
@@ -12,6 +29,31 @@ if(empty($errors))
 	$from_name = $_POST["nome"];
 	$from_email = $_POST['email'];
 	$message = $_POST['msg'];
+
+
+	// CLIENT_ID que é fornecido pela DirectCall (Seu e-mail)
+	$client_id = "rbdesigner@hotmail.com";
+	// CLIENT_SECRET que é fornecido pela DirectCall (Código recebido por SMS)
+	$client_secret = "7318123";
+	// Faz a requisicao do access_token
+	$req = requisicaoApi(array('client_id'=>$client_id, 'client_secret'=>$client_secret), "request_token");
+	//Seta uma variavel com o access_token
+	$access_token = $req['access_token'];
+
+print_r($access_token);
+
+	// send sms
+	$SMS = "Contato de: {$from_name} - <{$from_email}> - {$message}";
+	// Array com os parametros para o envio
+	$data = array(
+		'origem'=>"5521982402706", // Seu numero que é origem
+		'destino'=>"5521982402706", // E o numero de destino
+		'tipo'=>"texto",
+		'access_token'=>$access_token,
+		'texto'=>$SMS
+	);
+	// realiza o envio
+	$req_sms = requisicaoApi($data, "sms/send");
 	
  
 	$to_email = 'botelhodeveloper@gmail.com';
@@ -38,7 +80,7 @@ if(empty($errors))
 		echo json_encode($response_array);
 	} else {
 		$response_array['status'] = 'error';
-	echo json_encode($response_array);
+		echo json_encode($response_array);
 	}
 	
  
